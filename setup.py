2dcfd6f93af9bdb6dcb323e0ff5612fefcebe649
jortel <jortel@localhost>
2007-12-17 19:16:06 +0000

    added setup.cfg for missing .pyo problem when building rpms with python 2.4. update version to 1.1.1.

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+#!/usr/bin/python
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# written by: Jeff Ortel ( jortel@redhat.com )
+
 import sys
 from setuptools import setup, find_packages
 
@@ -10,7 +27,7 @@ else:
     
 setup(
     name="suds",
-    version="0.1",
+    version="0.1.1",
     description="Lightweight SOAP client",
     author="Jeff Ortel",
     author_email="jortel@redhat.com",
