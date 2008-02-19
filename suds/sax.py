deb25709b2e94505a06ff5fae80fcd3537e4a3cf
jortel <jortel@localhost>
2008-02-19 21:21:54 +0000

    updated docs

--- suds/sax.py
+++ suds/sax.py
@@ -24,41 +24,48 @@ class Attribute:
         self.value = value
         
     def resolvePrefix(self, prefix):
+        """ resolve the specified prefix to a known namespace """
         ns = (None,None)
         if self.parent is not None:
             ns = self.parent.resolvePrefix(prefix)
         return ns
     
     def qname(self):
+        """ get the fully qualified name """
         if self.prefix is None:
             return self.name
         else:
             return '%s:%s' % (self.prefix, self.name)
         
     def namespace(self):
+        """ get the attributes namespace """
         if self.prefix is None:
             return (None,None)
         else:
             return self.resolvePrefix(self.prefix)
         
     def splitPrefix(self, name):
+        """ split the name into a tuple (prefix, name) """
         if ':' in name:
-            return tuple(name.split(':'))
+            return tuple(name.split(':', 1))
         else:
             return (None, name)
     
     def __eq__(self, rhs):
+        """ equals operator """
         return rhs is not None and \
             isinstance(rhs, Attribute) and \
             self.prefix == rhs.name and \
             self.name == rhs.name
             
     def __repr__(self):
+        """ get a string representation """
         return \
             'attr (prefix=%s, name=%s, value=(%s))' %\
                 (self.prefix, self.name, self.value)
 
     def __str__(self):
+        """ get an xml string representation """
         return '%s="%s"' % (self.qname(), self.value)
 
 
@@ -75,6 +82,10 @@ class Element:
         self.applyns(ns)
             
     def append(self, child):
+        """
+        append the specified child based on whether it is an
+        element or an attrbuite.
+        """
         if isinstance(child, Element):
             self.children.append(child)
             child.parent = self
@@ -85,6 +96,7 @@ class Element:
             return
 
     def getChild(self, name, ns=None, default=None):
+        """ get a child by name and (optional) namespace """
         prefix, name = self.splitPrefix(name)
         if prefix is not None:
             ns = self.resolvePrefix(prefix)
@@ -95,6 +107,10 @@ class Element:
         return default
     
     def childAtPath(self, path):
+        """
+        get a child at the specifed path where path is a (/) separated
+        list of element names.
+        """
         result = None
         node = self
         for name in [p for p in path.split('/') if len(p) > 0]:
@@ -110,6 +126,10 @@ class Element:
         return result
 
     def childrenAtPath(self, path):
+        """
+        get a list of children at the specified path where path is a (/)
+        separated list of element names.
+        """
         result = []
         child = None
         parts = [p for p in path.split('/') if len(p) > 0]
@@ -146,6 +166,7 @@ class Element:
         return result
         
     def getChildren(self, name=None, ns=None):
+        """ get list of child elements by name and (optional) namespace """
         result = []
         prefix, name = self.splitPrefix(name)
         if prefix is not None:
@@ -158,12 +179,18 @@ class Element:
                 result.append(c)
         return result
     
-    def attribute(self, name, value=None, default=None):
-        attr = None
+    def attrib(self, name):
+        """ get an attribute by name """
+        result = None
         for a in self.attributes:
             if a.name == name:
-                attr = a
+                result = a
                 break
+        return result
+    
+    def attribute(self, name, value=None, default=None):
+        """ get/set an attribute by name """
+        attr = self.attrib(name)
         if value is None:
             if attr is not None:
                 if attr.value is None:
@@ -178,12 +205,14 @@ class Element:
                 attr.value = value
         
     def qname(self):
+        """ get the fully qualified name """
         if self.prefix is None:
             return self.name
         else:
             return '%s:%s' % (self.prefix, self.name)
         
     def namespace(self):
+        """ get the namespace """
         if self.prefix is None:
             p = self.parent
             while p is not None:
@@ -195,12 +224,14 @@ class Element:
             return self.resolvePrefix(self.prefix)
         
     def splitPrefix(self, name):
+        """ split the name in to a tuple (prefix, name) """
         if name is not None and ':' in name:
             return tuple(name.split(':', 1))
         else:
             return (None, name)
         
     def resolvePrefix(self, prefix):
+        """ resolve the specified prefix into a namespace """
         n = self
         while n is not None:
             if prefix in n.nsprefixes:
@@ -210,10 +241,12 @@ class Element:
         return (None,None)
             
     def isempty(self):
+        """ get whether the element has no children """
         return len(self.children) == 0 and \
             self.text is None
             
     def applyns(self, ns):
+        """ apply the namespace to this node """
         if ns is not None:
             if ns[0] is None:
                 self.expns = ns[1]
@@ -242,18 +275,13 @@ class Element:
             'element (prefix=%s, name=%s)' % (self.prefix, self.name)
     
     def nsdeclarations(self):
+        """ get namespace declarations """
         result = ''
         if self.expns is not None:
             result += ' xmlns="%s"' % self.expns
         for (p,u) in self.nsprefixes.items():
             result += ' xmlns:%s="%s"' % (p, u)
         return result
-    
-    def qname(self):
-        if self.prefix is None:
-            return self.name
-        else:
-            return '%s:%s' % (self.prefix, self.name)
     
     def __str__(self):
         return self.str()
@@ -361,6 +389,7 @@ class Parser:
     """ simple parser """
         
     def parse(self, file=None, url=None, string=None):
+        """ parse a document """
         handler = Handler()
         if file is not None:
             parse(file, handler)
