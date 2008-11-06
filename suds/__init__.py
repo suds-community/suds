80ad9e2e5cb3c5786808e56c2aecfa0a6f1ca9da
jortel <jortel@localhost>
2008-11-06 20:47:48 +0000

    Fix ticket #49, Builder updated to add missing type information to inner classes; marshaller fixed to issue warning and continue when Object has no type information instead of trying to push (type,None) into the resolver.

--- suds/__init__.py
+++ suds/__init__.py
@@ -29,7 +29,7 @@ import socket
 # Project properties
 #
 
-properties = dict(version='0.3.2', build="beta (R379-20081106)")
+properties = dict(version='0.3.2', build="beta (R380-20081106)")
 
 #
 # Exceptions
