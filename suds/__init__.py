7a6dec8a2d54ca6ee6e77e727b6e3a55fb705a40
jortel <jortel@localhost>
2009-03-06 23:03:55 +0000

    Update WSDL processing to handle <soap:operation/> inside of <wsdl:operation/> as optional.

--- suds/__init__.py
+++ suds/__init__.py
@@ -29,7 +29,7 @@ import socket
 # Project properties
 #
 
-properties = dict(version='0.3.5', build="(beta) R462-20090305")
+properties = dict(version='0.3.5', build="(beta) R464-20090306")
 
 #
 # Exceptions
