0b39cfa971d062857d6756d760e9c9d0c343b6cc
jortel <jortel@localhost>
2009-05-13 15:47:27 +0000

    bump beta release

--- suds/__init__.py
+++ suds/__init__.py
@@ -29,7 +29,7 @@ import socket
 # Project properties
 #
 
-properties = dict(version='0.3.6', build="(beta) R506-20090512")
+properties = dict(version='0.3.6', build="(beta) R507-20090513")
 
 #
 # Exceptions
