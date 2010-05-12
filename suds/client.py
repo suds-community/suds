ffa1314888f397a53f4612111281b568960c0ea2
jortel <jortel@localhost>
2010-05-12 16:00:22 +0000

    Pass Plugin.onSend() the soap envelope root Element instead of the Document.

--- suds/client.py
+++ suds/client.py
@@ -624,7 +624,7 @@ class SoapClient:
         try:
             self.last_sent(Document(msg))
             plugins = PluginContainer(self.options.plugins)
-            plugins.onSend(envelope=msg)
+            plugins.onSend(envelope=msg.root())
             request = Request(location, str(msg))
             request.headers = self.headers()
             reply = transport.send(request)
