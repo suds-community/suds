731ecd06cdce235d85cb95a0090229d35f7c264c
jortel <jortel@localhost>
2010-08-31 15:25:01 +0000

    Add option: 'prettyxml' default=0 to control output XML as pretty printed or not.

--- suds/sax/element.py
+++ suds/sax/element.py
@@ -767,6 +767,29 @@ class Element:
         result.append('</%s>' % self.qname())
         result = ''.join(result)
         return result
+    
+    def plain(self):
+        """
+        Get a string representation of this XML fragment.
+        @return: A I{plain} string.
+        @rtype: basestring
+        """
+        result = []
+        result.append('<%s' % self.qname())
+        result.append(self.nsdeclarations())
+        for a in [unicode(a) for a in self.attributes]:
+            result.append(' %s' % a)
+        if self.isempty():
+            result.append('/>')
+            return ''.join(result)
+        result.append('>')
+        if self.hasText():
+            result.append(self.text.escape())
+        for c in self.children:
+            result.append(c.plain())
+        result.append('</%s>' % self.qname())
+        result = ''.join(result)
+        return result
 
     def nsdeclarations(self):
         """
