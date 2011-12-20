fbdfbe4fbffb736a7a04fb3140717f8d36dc4f4b
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2011-12-20 23:09:57 +0100

    Minor stylistic version.py module comment change.

--- suds/version.py
+++ suds/version.py
@@ -14,10 +14,11 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 """
-Module containing the library's version information. This version information
-has been extracted into a separate file so it can be read from the setup.py
-script without having to import the suds package itself. See the setup.py
-script for more detailed information.
+Module containing the library's version information.
+
+  This version information has been extracted into a separate file so it can be
+read from the setup.py script without having to import the suds package itself.
+See the setup.py script for more detailed information.
 
 """
 
