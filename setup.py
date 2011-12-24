b56d0710de6e5901d87e959ffd8ded66c66adfc7
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2011-12-25 00:13:48 +0100

    Made the long_description project metadata more detailed.

--- setup.py
+++ setup.py
@@ -80,6 +80,15 @@ Lightweight SOAP client (Jurko's fork).
 
   Based on the original 'suds' project by Jeff Ortel (jortel at redhat
 dot com) hosted at 'https://fedorahosted.org/suds'.
+
+  'Suds' is a lightweight SOAP-based web service client for Python licensed
+under LGPL (see the LICENSE.txt file included in the distribution).
+
+  This is hopefully just a temporary fork of the original suds Python library
+project created because the original project development seems to have stalled.
+Should be reintegrated back into the original project if it ever gets revived
+again.
+
 """
 
 package_name = "suds-jurko"
