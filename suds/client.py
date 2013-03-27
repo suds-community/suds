a86c9c90a5197c5ab18c7e15f3c8809c18c6eeca
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2013-03-28 00:38:20 +0100

    Cleaned up code for sending XML requests.
    This is a minor refactoring on the way to correctly implementing processing SOAP
    errors so they get detected and reported consistently independent of whether the
    server returned HTTP status code 500 and/or whether suds has been configured to
    report them using exceptions or not. Currently suds detects SOAP <Fault>
    elements correctly if configured to raise them as exceptions but not otherwise.

--- suds/client.py
+++ suds/client.py
@@ -629,47 +629,43 @@ class SoapClient:
         @return: The reply to the sent message.
         @rtype: I{builtin} or I{subclass of} L{Object}
         """
-        result = None
         location = suds.bytes2str(self.location())
         output_binding = self.method.binding.output
         transport = self.options.transport
         retxml = self.options.retxml
         nosend = self.options.nosend
         prettyxml = self.options.prettyxml
-        timer = metrics.Timer()
         log.debug('sending to (%s)\nmessage:\n%s', location, soapenv)
+        self.last_sent(soapenv)
+        plugins = PluginContainer(self.options.plugins)
+        plugins.message.marshalled(envelope=soapenv.root())
+        if prettyxml:
+            soapenv = soapenv.str()
+        else:
+            soapenv = soapenv.plain()
+        soapenv = soapenv.encode('utf-8')
+        ctx = plugins.message.sending(envelope=soapenv)
+        soapenv = ctx.envelope
+        if nosend:
+            return RequestContext(self, output_binding, soapenv)
+        request = Request(location, soapenv)
+        request.headers = self.headers()
         try:
-            self.last_sent(soapenv)
-            plugins = PluginContainer(self.options.plugins)
-            plugins.message.marshalled(envelope=soapenv.root())
-            if prettyxml:
-                soapenv = soapenv.str()
-            else:
-                soapenv = soapenv.plain()
-            soapenv = soapenv.encode('utf-8')
-            ctx = plugins.message.sending(envelope=soapenv)
-            soapenv = ctx.envelope
-            if nosend:
-                return RequestContext(self, output_binding, soapenv)
-            request = Request(location, soapenv)
-            request.headers = self.headers()
+            timer = metrics.Timer()
             timer.start()
             reply = transport.send(request)
             timer.stop()
             metrics.log.debug('waited %s on server reply', timer)
-            ctx = plugins.message.received(reply=reply.message)
-            reply.message = ctx.reply
-            if retxml:
-                result = reply.message
-            else:
-                result = self.succeeded(output_binding, reply.message)
         except TransportError, e:
             if e.httpcode in (httplib.ACCEPTED, httplib.NO_CONTENT):
-                result = None
-            else:
-                log.error(self.last_sent())
-                result = self.failed(output_binding, e)
-        return result
+                return
+            log.error(self.last_sent())
+            return self.failed(output_binding, e)
+        ctx = plugins.message.received(reply=reply.message)
+        reply.message = ctx.reply
+        if retxml:
+            return reply.message
+        return self.succeeded(output_binding, reply.message)
 
     def headers(self):
         """
