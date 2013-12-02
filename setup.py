088efd877f1a49d21e4c3dd84b8d031012602f0c
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2013-12-02 21:06:50 +0100

    wrap package setup's long_description field to 72 characters
    As noted in an already existing comment, PKG_INFO package distribution metadata
    file stores this text with an 8 space indentation and therefore should not be
    longer than 72 characters.

--- setup.py
+++ setup.py
@@ -112,13 +112,14 @@ Lightweight SOAP client (Jurko's fork).
   Based on the original 'suds' project by Jeff Ortel (jortel at redhat
 dot com) hosted at 'https://fedorahosted.org/suds'.
 
-  'Suds' is a lightweight SOAP-based web service client for Python licensed
-under LGPL (see the LICENSE.txt file included in the distribution).
+  'Suds' is a lightweight SOAP-based web service client for Python
+licensed under LGPL (see the LICENSE.txt file included in the
+distribution).
 
-  This is hopefully just a temporary fork of the original suds Python library
-project created because the original project development seems to have stalled.
-Should be reintegrated back into the original project if it ever gets revived
-again.
+  This is hopefully just a temporary fork of the original suds Python
+library project created because the original project development seems
+to have stalled. Should be reintegrated back into the original project
+if it ever gets revived again.
 
 """
 
