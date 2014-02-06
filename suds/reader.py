1a0e33122738a2ff88023d63c82b3029a0fee70e
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2014-02-06 13:44:05 +0100

    clean up suds.reader module
    Replaced several from..import constructs with a simple imports.
    Reader class implemented as a new-style class.
    Docstring updates.
    Stylistic code & comment changes.
    
    Several private methods renamed:
      * DocumentReader.cache() --> DocumentReader.__cache()
      * DocumentReader.download() --> DocumentReader.__fetch()
      * DefinitionsReader.cache() --> DefinitionsReader.__cache()

--- suds/reader.py
+++ suds/reader.py
@@ -1,166 +1,189 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the (LGPL) GNU Lesser General Public License as
-# published by the Free Software Foundation; either version 3 of the
-# License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the (LGPL) GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library Lesser General Public License for more details at
-# ( http://www.gnu.org/licenses/lgpl.html ).
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Library Lesser General Public License
+# for more details at ( http://www.gnu.org/licenses/lgpl.html ).
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 # written by: Jeff Ortel ( jortel@redhat.com )
 
 """
-  XML document reader classes providing integration with the suds library's
+XML document reader classes providing integration with the suds library's
 caching system.
+
 """
 
-
-from suds.cache import Cache, NoCache
-from suds.plugin import PluginContainer
-from suds.sax.parser import Parser
-from suds.store import DocumentStore
-from suds.transport import Request
+import suds.cache
+import suds.plugin
+import suds.sax.parser
+import suds.transport
 
 
-class Reader:
+class Reader(object):
     """
     Provides integration with the cache.
+
     @ivar options: An options object.
     @type options: I{Options}
+
     """
 
     def __init__(self, options):
         """
         @param options: An options object.
         @type options: I{Options}
+
         """
         self.options = options
-        self.plugins = PluginContainer(options.plugins)
+        self.plugins = suds.plugin.PluginContainer(options.plugins)
 
     def mangle(self, name, x):
         """
         Mangle the name by hashing the I{name} and appending I{x}.
-        @return: the mangled name.
+
+        @return: The mangled name.
+        @rtype: str
+
         """
         h = abs(hash(name))
-        return '%s-%s' % (h, x)
+        return "%s-%s" % (h, x)
 
 
 class DocumentReader(Reader):
-    """
-    Provides integration between the SAX L{Parser} and the document cache.
-    """
+    """Integrates between the SAX L{Parser} and the document cache."""
 
     def open(self, url):
         """
         Open an XML document at the specified I{URL}.
-        First, the document attempted to be retrieved from the I{object cache}.
-        If not found, it is downloaded and parsed using the SAX parser. The
-        result is added to the cache for the next open().
+
+        First, a preparsed document is looked up in the I{object cache}. If not
+        found, its content is fetched from an external source and parsed using
+        the SAX parser. The result is cached for the next open().
+
         @param url: A document URL.
         @type url: str.
         @return: The specified XML document.
         @rtype: I{Document}
+
         """
-        cache = self.cache()
-        id = self.mangle(url, 'document')
+        cache = self.__cache()
+        id = self.mangle(url, "document")
         d = cache.get(id)
         if d is None:
-            d = self.download(url)
+            d = self.__fetch(url)
             cache.put(id, d)
         self.plugins.document.parsed(url=url, document=d.root())
         return d
 
-    def download(self, url):
+    def __cache(self):
         """
-        Download the document.
+        Get the I{object cache}.
+
+        @return: The I{cache} when I{cachingpolicy} = B{0}.
+        @rtype: L{Cache}
+
+        """
+        if self.options.cachingpolicy == 0:
+            return self.options.cache
+        return suds.cache.NoCache()
+
+    def __fetch(self, url):
+        """
+        Fetch document content from an external source.
+
+        The document content will first be looked up in the registered document
+        store, and if not found there, downloaded using the registered
+        transport system.
+
+        Before being returned, the fetched document content first gets
+        processed by all the registered 'loaded' plugins.
+
         @param url: A document URL.
         @type url: str.
-        @return: A file pointer to the document.
+        @return: A file pointer to the fetched document content.
         @rtype: file-like
+
         """
         content = None
         store = self.options.documentStore
         if store is not None:
             content = store.open(url)
         if content is None:
-            fp = self.options.transport.open(Request(url))
+            request = suds.transport.Request(url)
+            fp = self.options.transport.open(request)
             try:
                 content = fp.read()
             finally:
                 fp.close()
         ctx = self.plugins.document.loaded(url=url, document=content)
         content = ctx.document
-        sax = Parser()
+        sax = suds.sax.parser.Parser()
         return sax.parse(string=content)
-
-    def cache(self):
-        """
-        Get the cache.
-        @return: The I{cache} when I{cachingpolicy} = B{0}.
-        @rtype: L{Cache}
-        """
-        if self.options.cachingpolicy == 0:
-            return self.options.cache
-        return NoCache()
 
 
 class DefinitionsReader(Reader):
     """
-    Provides integration between the WSDL Definitions object and the object
-    cache.
-    @ivar fn: A factory function (constructor) used to
-        create the object not found in the cache.
+    Integrates between the WSDL Definitions object and the object cache.
+
+    @ivar fn: A factory function used to create objects not found in the cache.
     @type fn: I{Constructor}
+
     """
 
     def __init__(self, options, fn):
         """
         @param options: An options object.
         @type options: I{Options}
-        @param fn: A factory function (constructor) used to create the object
-            not found in the cache.
+        @param fn: A factory function used to create objects not found in the
+            cache.
         @type fn: I{Constructor}
+
         """
-        Reader.__init__(self, options)
+        super(DefinitionsReader, self).__init__(options)
         self.fn = fn
 
     def open(self, url):
         """
-        Open a WSDL at the specified I{URL}.
-        First, the WSDL attempted to be retrieved from
-        the I{object cache}.  After unpickled from the cache, the
-        I{options} attribute is restored.
-        If not found, it is downloaded and instantiated using the
-        I{fn} constructor and added to the cache for the next open().
+        Open a WSDL schema at the specified I{URL}.
+
+        First, the WSDL schema is looked up in the I{object cache}. If not
+        found, a new one constructed using the I{fn} factory function and the
+        result is cached for the next open().
+
         @param url: A WSDL URL.
         @type url: str.
         @return: The WSDL object.
         @rtype: I{Definitions}
+
         """
-        cache = self.cache()
-        id = self.mangle(url, 'wsdl')
+        cache = self.__cache()
+        id = self.mangle(url, "wsdl")
         d = cache.get(id)
         if d is None:
             d = self.fn(url, self.options)
             cache.put(id, d)
         else:
+            # Cached WSDL Definitions objects may have been created with
+            # different options so we update them here with our current ones.
             d.options = self.options
             for imp in d.imports:
                 imp.imported.options = self.options
         return d
 
-    def cache(self):
+    def __cache(self):
         """
-        Get the cache.
+        Get the I{object cache}.
+
         @return: The I{cache} when I{cachingpolicy} = B{1}.
         @rtype: L{Cache}
+
         """
         if self.options.cachingpolicy == 1:
             return self.options.cache
-        return NoCache()
+        return suds.cache.NoCache()
