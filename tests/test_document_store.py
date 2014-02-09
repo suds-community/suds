700574ad53bf85efd9ed4789a3f0dbf869e85da1
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2014-02-09 09:54:52 +0100

    test code cleanup
    Stylistic code, comment & docstring changes.
    
    test_document_store.py:
    * Made private module functions use a single underscore name prefix.
    
    test_transport_http.py:
    * Removed an unnecessary package import.
    * Made an internal _check_Authorization_header() test function more localized.

--- tests/test_document_store.py
+++ tests/test_document_store.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the (LGPL) GNU Lesser General Public License as
-# published by the Free Software Foundation; either version 3 of the
-# License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the (LGPL) GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library Lesser General Public License for more details at
-# ( http://www.gnu.org/licenses/lgpl.html ).
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Library Lesser General Public License
+# for more details at ( http://www.gnu.org/licenses/lgpl.html ).
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 # written by: Jurko Gospodnetić ( jurko.gospodnetic@pke.hr )
 
 """
@@ -39,33 +38,33 @@ def test_accessing_DocumentStore_content():
     content2 = suds.byte_str("two")
     content1_1 = suds.byte_str("one one")
 
-    store = suds.store.DocumentStore({"1":content1})
+    store = suds.store.DocumentStore({"1": content1})
     assert len(store) == 2
-    __test_default_DocumentStore_content(store)
-    __test_open(store, "1", content1)
+    _test_default_DocumentStore_content(store)
+    _test_open(store, "1", content1)
 
-    store = suds.store.DocumentStore({"1":content1, "2":content2})
+    store = suds.store.DocumentStore({"1": content1, "2": content2})
     assert len(store) == 3
-    __test_default_DocumentStore_content(store)
-    __test_open(store, "1", content1)
-    __test_open(store, "2", content2)
+    _test_default_DocumentStore_content(store)
+    _test_open(store, "1", content1)
+    _test_open(store, "2", content2)
 
     store = suds.store.DocumentStore(uno=content1, due=content2)
     assert len(store) == 3
-    __test_default_DocumentStore_content(store)
-    __test_open(store, "uno", content1)
-    __test_open(store, "due", content2)
+    _test_default_DocumentStore_content(store)
+    _test_open(store, "uno", content1)
+    _test_open(store, "due", content2)
 
-    store = suds.store.DocumentStore({"1 1":content1_1})
+    store = suds.store.DocumentStore({"1 1": content1_1})
     assert len(store) == 2
-    __test_default_DocumentStore_content(store)
-    __test_open(store, "1 1", content1_1)
+    _test_default_DocumentStore_content(store)
+    _test_open(store, "1 1", content1_1)
 
-    store = suds.store.DocumentStore({"1":content1, "1 1":content1_1})
+    store = suds.store.DocumentStore({"1": content1, "1 1": content1_1})
     assert len(store) == 3
-    __test_default_DocumentStore_content(store)
-    __test_open(store, "1", content1)
-    __test_open(store, "1 1", content1_1)
+    _test_default_DocumentStore_content(store)
+    _test_open(store, "1", content1)
+    _test_open(store, "1 1", content1_1)
 
 
 def test_accessing_missing_DocumentStore_content():
@@ -80,7 +79,7 @@ def test_accessing_missing_DocumentStore_content():
 
 def test_default_DocumentStore_instance():
     assert len(suds.store.defaultDocumentStore) == 1
-    __test_default_DocumentStore_content(suds.store.defaultDocumentStore)
+    _test_default_DocumentStore_content(suds.store.defaultDocumentStore)
 
 
 def test_empty_DocumentStore_instance_is_not_shared():
@@ -95,43 +94,43 @@ def test_updating_DocumentStore_content():
 
     store = suds.store.DocumentStore()
     assert len(store) == 1
-    __test_default_DocumentStore_content(store)
+    _test_default_DocumentStore_content(store)
 
-    store.update({"1":content1})
+    store.update({"1": content1})
     assert len(store) == 2
-    __test_default_DocumentStore_content(store)
-    __test_open(store, "1", content1)
+    _test_default_DocumentStore_content(store)
+    _test_open(store, "1", content1)
 
-    store.update({"1":content1, "2":content2, "1 1":content1_1})
+    store.update({"1": content1, "2": content2, "1 1": content1_1})
     assert len(store) == 4
-    __test_default_DocumentStore_content(store)
-    __test_open(store, "1", content1)
-    __test_open(store, "2", content2)
-    __test_open(store, "1 1", content1_1)
+    _test_default_DocumentStore_content(store)
+    _test_open(store, "1", content1)
+    _test_open(store, "2", content2)
+    _test_open(store, "1 1", content1_1)
 
-    store.update({"2":content2, "1 1":content1_1})
+    store.update({"2": content2, "1 1": content1_1})
     assert len(store) == 4
-    __test_default_DocumentStore_content(store)
-    __test_open(store, "1", content1)
-    __test_open(store, "2", content2)
-    __test_open(store, "1 1", content1_1)
+    _test_default_DocumentStore_content(store)
+    _test_open(store, "1", content1)
+    _test_open(store, "2", content2)
+    _test_open(store, "1 1", content1_1)
 
     store.update(uno=content1, due=content2)
     assert len(store) == 6
-    __test_default_DocumentStore_content(store)
-    __test_open(store, "1", content1)
-    __test_open(store, "2", content2)
-    __test_open(store, "1 1", content1_1)
-    __test_open(store, "uno", content1)
-    __test_open(store, "due", content2)
+    _test_default_DocumentStore_content(store)
+    _test_open(store, "1", content1)
+    _test_open(store, "2", content2)
+    _test_open(store, "1 1", content1_1)
+    _test_open(store, "uno", content1)
+    _test_open(store, "due", content2)
 
 
-def __test_default_DocumentStore_content(store):
-    __test_open(store, "schemas.xmlsoap.org/soap/encoding/",
+def _test_default_DocumentStore_content(store):
+    _test_open(store, "schemas.xmlsoap.org/soap/encoding/",
         suds.store.soap5_encoding_schema)
 
 
-def __test_open(store, location, expected_content):
+def _test_open(store, location, expected_content):
     assert store.open(location) is expected_content
     assert store.open("buga-wuga://%s" % location) is expected_content
     assert store.open("ftp://%s" % location) is expected_content
