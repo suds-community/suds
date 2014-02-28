bd80be65c95e22b686340e5921b676d597bb5042
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2014-02-28 01:06:22 +0100

    clean up the suds.sax.parser module
    Minor code refactoring.
    Used double instead of single quotes in string literals.
    Docstring & comment cleanup.

--- suds/sax/parser.py
+++ suds/sax/parser.py
@@ -1,28 +1,27 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the (LGPL) GNU Lesser General Public License as
-# published by the Free Software Foundation; either version 3 of the
-# License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the (LGPL) GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library Lesser General Public License for more details at
-# ( http://www.gnu.org/licenses/lgpl.html ).
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Library Lesser General Public License
+# for more details at ( http://www.gnu.org/licenses/lgpl.html ).
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 # written by: Jeff Ortel ( jortel@redhat.com )
 
 """
-The sax module contains a collection of classes that provide a
-(D)ocument (O)bject (M)odel representation of an XML document.
-The goal is to provide an easy, intuitive interface for managing XML
-documents.  Although, the term, DOM, is used above, this model is
-B{far} better.
+Classes providing a (D)ocument (O)bject (M)odel representation of an XML
+document.
 
-XML namespaces in suds are represented using a (2) element tuple
-containing the prefix and the URI.  Eg: I{('tns', 'http://myns')}
+The goal is to provide an easy, intuitive interface for managing XML documents.
+Although the term DOM is used above, this model is B{far} better.
+
+XML namespaces in suds are represented using a (2) element tuple containing the
+prefix and the URI, e.g. I{('tns', 'http://myns')}.
 
 """
 
@@ -40,7 +39,7 @@ from xml.sax.handler import feature_external_ges
 
 
 class Handler(ContentHandler):
-    """ sax hanlder """
+    """SAX handler."""
 
     def __init__(self):
         self.nodes = [Document()]
@@ -51,7 +50,7 @@ class Handler(ContentHandler):
         for a in attrs.getNames():
             n = unicode(a)
             v = unicode(attrs.getValue(a))
-            attribute = Attribute(n,v)
+            attribute = Attribute(n, v)
             if self.mapPrefix(node, attribute):
                 continue
             node.append(attribute)
@@ -60,29 +59,26 @@ class Handler(ContentHandler):
         self.push(node)
 
     def mapPrefix(self, node, attribute):
-        skip = False
-        if attribute.name == 'xmlns':
+        if attribute.name == "xmlns":
             if len(attribute.value):
                 node.expns = unicode(attribute.value)
-            skip = True
-        elif attribute.prefix == 'xmlns':
+            return True
+        if attribute.prefix == "xmlns":
             prefix = attribute.name
             node.nsprefixes[prefix] = unicode(attribute.value)
-            skip = True
-        return skip
+            return True
+        return False
 
     def endElement(self, name):
         name = unicode(name)
-        current = self.top()
-        if len(current.charbuffer):
-            current.text = Text(u''.join(current.charbuffer))
+        current = self.pop()
+        if name != current.qname():
+            raise Exception("malformed document")
+        if current.charbuffer:
+            current.text = Text(u"".join(current.charbuffer))
         del current.charbuffer
-        if len(current):
+        if current:
             current.trim()
-        if name == current.qname():
-            self.pop()
-        else:
-            raise Exception('malformed document')
 
     def characters(self, content):
         text = unicode(content)
@@ -97,11 +93,11 @@ class Handler(ContentHandler):
         return self.nodes.pop()
 
     def top(self):
-        return self.nodes[len(self.nodes)-1]
+        return self.nodes[-1]
 
 
 class Parser:
-    """ SAX Parser """
+    """SAX parser."""
 
     @classmethod
     def saxparser(cls):
@@ -109,28 +105,33 @@ class Parser:
         p.setFeature(feature_external_ges, 0)
         h = Handler()
         p.setContentHandler(h)
-        return (p, h)
+        return p, h
 
     def parse(self, file=None, string=None):
         """
         SAX parse XML text.
+
         @param file: Parse a python I{file-like} object.
-        @type file: I{file-like} object.
+        @type file: I{file-like} object
         @param string: Parse string XML.
         @type string: str
+        @return: Parsed XML document.
+        @rtype: L{Document}
+
         """
+        if file is None and string is None:
+            return
         timer = suds.metrics.Timer()
         timer.start()
-        sax, handler = self.saxparser()
-        if file is not None:
-            sax.parse(file)
-            timer.stop()
-            suds.metrics.log.debug('sax (%s) duration: %s', file, timer)
-            return handler.nodes[0]
-        if string is not None:
+        source = file
+        if file is None:
             source = InputSource(None)
             source.setByteStream(suds.BytesIO(string))
-            sax.parse(source)
-            timer.stop()
-            suds.metrics.log.debug('%s\nsax duration: %s', string, timer)
-            return handler.nodes[0]
+        sax, handler = self.saxparser()
+        sax.parse(source)
+        timer.stop()
+        if file is None:
+            suds.metrics.log.debug("%s\nsax duration: %s", string, timer)
+        else:
+            suds.metrics.log.debug("sax (%s) duration: %s", file, timer)
+        return handler.nodes[0]
