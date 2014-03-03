58754d1c6c5a3dfe43ab4088d8f6b7d3aba026eb
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2014-03-03 18:50:31 +0100

    fix inter-process cache reuse with Python's hash randomization
    Updated how suds constructs its cached WSDL & XML identifiers to allow cached
    data reuse between different processes with Python's hash randomization feature
    enabled.
    
    Previously constructed using the built-in Python hash() function, while now it
    gets constructed using md5 hash.
    
    Python's hash randomization (implemented since Python 2.6.8, enabled by default
    since Python 3.3) was causing different processes to mangle their cached data
    names differently.
    
    Many thanks to Eugene Yakubovich for reporting the issue as well as providing
    the initial fix & tests.

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -30,6 +30,7 @@ if __name__ == "__main__":
 import suds
 import suds.options
 import suds.reader
+import tests
 
 
 class TestCacheItemNameMangling:
@@ -43,6 +44,33 @@ class TestCacheItemNameMangling:
         mangled1 = reader.mangle(test_item_name1, test_item_suffix)
         mangled2 = reader.mangle(test_item_name2, test_item_suffix)
         assert mangled1 != mangled2
+
+    def test_inter_processes_persistence(self, tmpdir):
+        """
+        Same cache item names must be mangled the same in different processes.
+
+        This is a regression test against using a built-in Python hash()
+        function internally since that function may be seeded by a process
+        specific random seed. This Python interpreter behaviour has been
+        enabled by default since Python 3.3 and may be explicitly enabled on
+        earlier Python interpreter versions as well.
+
+        """
+        test_item_name = "test string"
+        test_item_suffix = "test suffix"
+        reader = suds.reader.Reader(suds.options.Options())
+        expected = reader.mangle(test_item_name, test_item_suffix)
+        test_file = tmpdir.join("test_mangle.py")
+        test_file.write("""
+import suds.options
+import suds.reader
+reader = suds.reader.Reader(suds.options.Options())
+mangled = reader.mangle("%(test_item_name)s", "%(test_item_suffix)s")
+assert mangled == '%(expected)s'
+""" % {"expected": expected,
+    "test_item_name": test_item_name,
+    "test_item_suffix": test_item_suffix})
+        tests.run_test_process(test_file)
 
     def test_repeatable__different_readers(self):
         test_item_name = "R2D2"
