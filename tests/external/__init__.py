b547c642f71fdfd20ec05abc249e62012a93bf7a
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2014-05-12 11:02:17 +0200

    fix installation on Python 3.1 - project source files containing UTF-8 BOM
    py2to3 does not support reading source files with an explicitly specified UTF-8
    BOM prior to Python 3.2. This was causing errors when using it to process two
    of our project files.
    
    Updated relevant HACKING.rst documentation.

--- tests/external/__init__.py
+++ tests/external/__init__.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the (LGPL) GNU Lesser General Public License as published by the
