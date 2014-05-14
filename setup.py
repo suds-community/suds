c3bcb5e8054234d7828bd95835aee8e7173614c5
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2014-05-14 14:45:37 +0200

    make setup.py header comment consistent with other utf-8 encoded modules
    In all other suds project's utf-8 encoded modules, the coding comment is
    separated from the copyright comment with a single empty line.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
+
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the (LGPL) GNU Lesser General Public License as published by the
 # Free Software Foundation; either version 3 of the License, or (at your
