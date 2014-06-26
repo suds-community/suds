31de2268422dc054cf7c38132a87a878ca2261fd
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2014-06-26 15:28:50 +0200

    clean up the suds.xsd.deplist module - stylistic changes
    PEP8-ified the source code.
    Corrected invalid code comments.
    add() operation no longer returns self.
    Internal operations & data marked as private.

--- suds/xsd/deplist.py
+++ suds/xsd/deplist.py
@@ -1,21 +1,22 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the (LGPL) GNU Lesser General Public License as
-# published by the Free Software Foundation; either version 3 of the
-# License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the (LGPL) GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library Lesser General Public License for more details at
-# ( http://www.gnu.org/licenses/lgpl.html ).
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Library Lesser General Public License
+# for more details at ( http://www.gnu.org/licenses/lgpl.html ).
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 # written by: Jeff Ortel ( jortel@redhat.com )
 
 """
-The I{depsolve} module defines a class for performing dependency solving.
+The I{deplist} module defines a class for performing topological dependency
+sorting - dependency entries before those that depend on them.
+
 """
 
 from suds import *
@@ -27,104 +28,103 @@ log = getLogger(__name__)
 class DepList:
     """
     Dependency solving list.
+
     Items are tuples: (object, (deps,))
-    @ivar raw: The raw (unsorted) items.
-    @type raw: list
-    @ivar index: The index of (unsorted) items.
-    @type index: list
-    @ivar stack: The sorting stack.
-    @type stack: list
-    @ivar pushed: The I{pushed} set tracks items that have been
-        processed.
-    @type pushed: set
-    @ivar sorted: The sorted list of items.
-    @type sorted: list
+
+    @ivar __unsorted: The raw (unsorted) items.
+    @type __unsorted: list
+    @ivar __index: The index of (unsorted) items.
+    @type __index: dict
+    @ivar __stack: The sorting stack.
+    @type __stack: list
+    @ivar __pushed: The I{pushed} set tracks items that have been processed.
+    @type __pushed: set
+
     """
 
     def __init__(self):
-        """ """
-        self.unsorted = []
-        self.index = {}
-        self.stack = []
-        self.pushed = set()
-        self.sorted = None
+        self.__unsorted = []
+        self.__index = {}
+        self.__stack = []
+        self.__pushed = set()
 
     def add(self, *items):
         """
         Add items to be sorted.
+
         @param items: One or more items to be added.
         @type items: I{item}
-        @return: self
-        @rtype: L{DepList}
+
         """
         for item in items:
-            self.unsorted.append(item)
+            self.__unsorted.append(item)
             key = item[0]
-            self.index[key] = item
-        return self
+            self.__index[key] = item
 
     def sort(self):
         """
         Sort the list based on dependencies.
+
         @return: The sorted items.
         @rtype: list
+
         """
-        self.sorted = list()
-        self.pushed = set()
-        for item in self.unsorted:
-            popped = []
-            self.push(item)
-            while len(self.stack):
+        sorted = []
+        self.__pushed = set()
+        for item in self.__unsorted:
+            self.__push(item)
+            while self.__stack:
                 try:
-                    top = self.top()
+                    top = self.__top()
                     ref = top[1].next()
-                    refd = self.index.get(ref)
+                    refd = self.__index.get(ref)
                     if refd is None:
                         log.debug('"%s" not found, skipped', Repr(ref))
                         continue
-                    self.push(refd)
+                    self.__push(refd)
                 except StopIteration:
-                    popped.append(self.pop())
+                    sorted.append(self.__pop())
                     continue
-            for p in popped:
-                self.sorted.append(p)
-        self.unsorted = self.sorted
-        return self.sorted
+        self.__unsorted = sorted
+        return sorted
 
-    def top(self):
+    def __pop(self):
         """
-        Get the item at the top of the stack.
-        @return: The top item.
-        @rtype: (item, iter)
-        """
-        return self.stack[-1]
+        Pop & return the top item off the sorting stack.
 
-    def push(self, item):
-        """
-        Push and item onto the sorting stack.
-        @param item: An item to push.
-        @type item: I{item}
-        @return: The number of items pushed.
-        @rtype: int
-        """
-        if item in self.pushed:
-            return
-        frame = (item, iter(item[1]))
-        self.stack.append(frame)
-        self.pushed.add(item)
-
-    def pop(self):
-        """
-        Pop the top item off the stack and append
-        it to the sorted list.
         @return: The popped item.
         @rtype: I{item}
+
         """
-        try:
-            frame = self.stack.pop()
-            return frame[0]
-        except Exception:
-            pass
+        return self.__stack.pop()[0]
+
+    def __push(self, item):
+        """
+        Push an item onto the sorting stack.
+
+        Each item is pushed as a stack frame 2-tuple containing:
+          - the item itself,
+          - an iterator over all of the item's dependencies
+
+        @param item: An item to push.
+        @type item: I{item}
+
+        """
+        if item in self.__pushed:
+            return
+        frame = (item, iter(item[1]))
+        self.__stack.append(frame)
+        self.__pushed.add(item)
+
+    def __top(self):
+        """
+        Get the top sorting stack frame.
+
+        @return: The top sorting stack frame.
+        @rtype: (I{item}, iter)
+
+        """
+        return self.__stack[-1]
 
 
 if __name__ == '__main__':
