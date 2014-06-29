d7fb18081fa63540e757c2db0e2c0a6a0a7cec4f
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2014-06-29 20:55:49 +0200

    update XSD element tests to with corrected form_qualified expectations
    Our original research was based on superficial Internet resources and lead us
    down the wrong path. Reference & top-level XSD elements are always considered
    qualified and that fact is not affected by any `form` or `elementFormDefault`
    attribute values.

--- tests/test_xsd_element.py
+++ tests/test_xsd_element.py
@@ -37,101 +37,58 @@ import pytest
 from six import b
 
 
-@pytest.mark.parametrize("form_default, form, expected", (
-    # default - not explicitly specified anywhere
-    (None, None, False),
+# shared test input data
+form_test_values = (None, "qualified", "unqualified", "invalid", "")
 
-    # specified on the element only
-    (None, "qualified", True),
-    (None, "unqualified", False),
-    (None, "invalid", False),
-    (None, "", False),
 
-    # specified on the schema only
-    ("qualified", None, True),
-    ("unqualified", None, False),
-    ("invalid", None, False),
-    ("", None, False),
+class TestElementForm:
+    """Test whether specific XSD elements are considered qualified."""
 
-    # specified on the schema, overruled on the element
-    ("qualified", "qualified", True),
-    ("qualified", "unqualified", False),
-    ("qualified", "invalid", False),
-    ("qualified", "", False),
-    ("unqualified", "qualified", True),
-    ("unqualified", "unqualified", False),
-    ("unqualified", "invalid", False),
-    ("unqualified", "", False),
-    ("invalid", "qualified", True),
-    ("invalid", "unqualified", False),
-    ("invalid", "invalid", False),
-    ("invalid", "", False),
-    ("", "qualified", True),
-    ("", "unqualified", False),
-    ("", "invalid", False),
-    ("", "", False)))
-def test_element_form__non_ref(form_default, form, expected):
-    """Test XSD non-ref element's form setting."""
-    element_name = "Elemento"
-    namespace = "tns"
-    schema_xml = """\
+    @pytest.mark.parametrize("form_default, form", [(x, y)
+        for x in form_test_values
+        for y in form_test_values])
+    def test_local_element(self, form_default, form):
+        parent_name = "Parent"
+        element_name = "Elemento"
+        namespace = "tns"
+        schema_xml = """\
 <?xml version='1.0' encoding='UTF-8'?>
 <schema xmlns="http://www.w3.org/2001/XMLSchema"
         targetNamespace="%(namespace)s"%(form_default)s>
-    <element name="%(element_name)s"%(form)s/>
+    <element name="%(parent_name)s">
+        <complexType>
+            <sequence>
+                <element name="%(element_name)s"%(form)s/>
+            </sequence>
+        </complexType>
+    </element>
 </schema>""" % {
-        "namespace": namespace,
-        "element_name": element_name,
-        "form": _attribute_xml("form", form),
-        "form_default": _attribute_xml("elementFormDefault", form_default)}
-    schema = _parse_schema_xml(b(schema_xml))
-    element = schema.elements[element_name, namespace]
-    assert bool(element.form_qualified) == expected
+            "element_name": element_name,
+            "form": _attribute_xml("form", form),
+            "form_default": _attribute_xml("elementFormDefault", form_default),
+            "namespace": namespace,
+            "parent_name": parent_name}
+        expected = form == "qualified" or (
+            (form is None) and (form_default == "qualified"))
+        schema = _parse_schema_xml(b(schema_xml))
+        parent_element = schema.elements[parent_name, namespace]
+        element = parent_element.get_child(element_name)[0]
+        assert bool(element.form_qualified) == expected
 
-
-@pytest.mark.parametrize("form_referenced, form_referencing, expected", (
-    # default - not explicitly specified anywhere
-    (None, None, False),
-
-    # specified on the referencing only
-    (None, "qualified", False),
-    (None, "unqualified", False),
-    (None, "invalid", False),
-    (None, "", False),
-
-    # specified on the referenced only
-    ("qualified", None, True),
-    ("unqualified", None, False),
-    ("invalid", None, False),
-    ("", None, False),
-
-    # specified on the referencing, overruled on the element
-    ("qualified", "qualified", True),
-    ("qualified", "unqualified", True),
-    ("qualified", "invalid", True),
-    ("qualified", "", True),
-    ("unqualified", "qualified", False),
-    ("unqualified", "unqualified", False),
-    ("unqualified", "invalid", False),
-    ("unqualified", "", False),
-    ("invalid", "qualified", False),
-    ("invalid", "unqualified", False),
-    ("invalid", "invalid", False),
-    ("invalid", "", False),
-    ("", "qualified", False),
-    ("", "unqualified", False),
-    ("", "invalid", False),
-    ("", "", False)))
-def test_element_form__ref(form_referenced, form_referencing, expected):
-    """Test XSD ref element's form setting."""
-    referencing_parent_name = "Referencing"
-    referenced_name = "Referenced"
-    namespace = "tns"
-    schema_xml = """\
+    @pytest.mark.parametrize("form_default, form, form_referenced", [(x, y, z)
+        for x in form_test_values
+        for y in form_test_values
+        for z in form_test_values])
+    def test_reference_to_internal(self, form_default, form, form_referenced):
+        """Reference element to an element in the same schema."""
+        referenced_name = "Referenced"
+        referencing_parent_name = "Referencing"
+        namespace = "tns"
+        schema_xml = """\
 <?xml version='1.0' encoding='UTF-8'?>
 <schema xmlns="http://www.w3.org/2001/XMLSchema"
-        targetNamespace="%(namespace)s"
-        xmlns:tns="%(namespace)s">
+        xmlns:tns="%(namespace)s"
+        targetNamespace="%(namespace)s"%(form_default)s>
     <element name="%(referenced_name)s"%(form_referenced)s/>
     <element name="%(referencing_parent_name)s">
         <complexType>
@@ -141,28 +98,22 @@ def test_element_form__ref(form_referenced, form_referencing, expected):
         </complexType>
     </element>
 </schema>""" % {
-        "namespace": namespace,
-        "referenced_name": referenced_name,
-        "referencing_parent_name": referencing_parent_name,
-        "form_referenced": _attribute_xml("form", form_referenced),
-        "form_referencing": _attribute_xml("form", form_referencing)}
-    schema = _parse_schema_xml(b(schema_xml))
-    referenced_element = schema.elements[referenced_name, namespace]
-    referencing_parent = schema.elements[referencing_parent_name, namespace]
-    referencing_element = referencing_parent.get_child(referenced_name)[0]
-    assert referenced_element.ref is None
-    assert referencing_element.ref == (referenced_name, namespace)
-    assert bool(referenced_element.form_qualified) == expected
-    assert bool(referencing_element.form_qualified) == expected
+            "form_default": _attribute_xml("elementFormDefault", form_default),
+            "form_referenced": _attribute_xml("form", form_referenced),
+            "form_referencing": _attribute_xml("form", form),
+            "namespace": namespace,
+            "referenced_name": referenced_name,
+            "referencing_parent_name": referencing_parent_name}
+        schema = _parse_schema_xml(b(schema_xml))
+        parent_element = schema.elements[referencing_parent_name, namespace]
+        referencing_element = parent_element.get_child(referenced_name)[0]
+        assert referencing_element.form_qualified
 
-
-def test_element_form__ref_from_different_schema__to_qualified():
-    """Referencing a qualified element in a separate schema."""
-    schema_xml_here = """\
+    def test_reference_to_external(self):
+        """Reference element to an element in an external schema."""
+        schema_xml_here = """\
 <?xml version='1.0' encoding='UTF-8'?>
-<schema xmlns="http://www.w3.org/2001/XMLSchema"
-        xmlns:there="ns-there"
-        targetNamespace="ns-here">
+<schema xmlns="http://www.w3.org/2001/XMLSchema" xmlns:there="ns-there">
     <import namespace="ns-there" schemaLocation="suds://there.xsd"/>
     <element name="Referencing">
         <complexType>
@@ -172,58 +123,44 @@ def test_element_form__ref_from_different_schema__to_qualified():
         </complexType>
     </element>
 </schema>"""
-    schema_xml_there = """\
-<?xml version='1.0' encoding='UTF-8'?>
-<schema xmlns="http://www.w3.org/2001/XMLSchema" targetNamespace="ns-there">
-    <element name="Referenced" form="qualified"/>
-</schema>"""
-    store = suds.store.DocumentStore({"there.xsd": b(schema_xml_there)})
-    schema = _parse_schema_xml(b(schema_xml_here), store)
-    referenced_element = schema.elements["Referenced", "ns-there"]
-    referencing_parent = schema.elements["Referencing", "ns-here"]
-    referencing_element = referencing_parent.get_child("Referenced")[0]
-    assert referenced_element.form_qualified
-    assert referencing_element.form_qualified
-
-
-def test_element_form__ref_from_different_schema__to_unqualified():
-    """Referencing an unqualified element in a separate schema."""
-    schema_xml_here = """\
-<?xml version='1.0' encoding='UTF-8'?>
-<schema xmlns="http://www.w3.org/2001/XMLSchema"
-        xmlns:there="ns-there"
-        elementFormDefault="qualified"
-        targetNamespace="ns-here">
-    <import namespace="ns-there" schemaLocation="suds://there.xsd"/>
-    <element name="Referencing">
-        <complexType>
-            <sequence>
-                <element ref="there:Referenced"/>
-            </sequence>
-        </complexType>
-    </element>
-</schema>"""
-    schema_xml_there = """\
+        schema_xml_there = """\
 <?xml version='1.0' encoding='UTF-8'?>
 <schema xmlns="http://www.w3.org/2001/XMLSchema" targetNamespace="ns-there">
     <element name="Referenced"/>
 </schema>"""
-    store = suds.store.DocumentStore({"there.xsd": b(schema_xml_there)})
-    schema = _parse_schema_xml(b(schema_xml_here), store)
-    referenced_element = schema.elements["Referenced", "ns-there"]
-    referencing_parent = schema.elements["Referencing", "ns-here"]
-    referencing_element = referencing_parent.get_child("Referenced")[0]
-    assert not referenced_element.form_qualified
-    assert not referencing_element.form_qualified
+        store = suds.store.DocumentStore({"there.xsd": b(schema_xml_there)})
+        schema = _parse_schema_xml(b(schema_xml_here), store)
+        referenced_element = schema.elements["Referenced", "ns-there"]
+        referencing_parent = schema.elements["Referencing", None]
+        referencing_element = referencing_parent.get_child("Referenced")[0]
+        assert referencing_element.form_qualified
+
+    @pytest.mark.parametrize("form_default, form", [(x, y)
+        for x in form_test_values
+        for y in form_test_values])
+    def test_top_level_element(self, form_default, form):
+        element_name = "Elemento"
+        namespace = "tns"
+        schema_xml = """\
+<?xml version='1.0' encoding='UTF-8'?>
+<schema xmlns="http://www.w3.org/2001/XMLSchema"
+        targetNamespace="%(namespace)s"%(form_default)s>
+    <element name="%(element_name)s"%(form)s/>
+</schema>""" % {
+            "element_name": element_name,
+            "form": _attribute_xml("form", form),
+            "form_default": _attribute_xml("elementFormDefault", form_default),
+            "namespace": namespace}
+        schema = _parse_schema_xml(b(schema_xml))
+        element = schema.elements[element_name, namespace]
+        assert element.form_qualified
 
 
 def test_reference():
     """Reference to an element in a different schema."""
     schema_xml_here = """\
 <?xml version='1.0' encoding='UTF-8'?>
-<schema xmlns="http://www.w3.org/2001/XMLSchema"
-        xmlns:there="ns-there"
-        targetNamespace="ns-here">
+<schema xmlns="http://www.w3.org/2001/XMLSchema" xmlns:there="ns-there">
     <import namespace="ns-there" schemaLocation="suds://there.xsd"/>
     <element name="Referencing">
         <complexType>
@@ -241,7 +178,7 @@ def test_reference():
     store = suds.store.DocumentStore({"there.xsd": b(schema_xml_there)})
     schema = _parse_schema_xml(b(schema_xml_here), store)
     referenced_element = schema.elements["Referenced", "ns-there"]
-    referencing_parent = schema.elements["Referencing", "ns-here"]
+    referencing_parent = schema.elements["Referencing", None]
     referencing_element = referencing_parent.get_child("Referenced")[0]
     assert referenced_element.ref is None
     assert referencing_element.ref == ("Referenced", "ns-there")
