47d029dadd35261d285595d2d1c3fbec7868e357
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2014-07-02 20:42:25 +0200

    tidy up up the suds.sax module (stylistic)
    - PEP-8ified comments
    - used double quotes consistently

--- suds/sax/__init__.py
+++ suds/sax/__init__.py
@@ -1,24 +1,23 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the (LGPL) GNU Lesser General Public License as
-# published by the Free Software Foundation; either version 3 of the
-# License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the (LGPL) GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library Lesser General Public License for more details at
-# ( http://www.gnu.org/licenses/lgpl.html ).
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Library Lesser General Public License
+# for more details at ( http://www.gnu.org/licenses/lgpl.html ).
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 # written by: Jeff Ortel ( jortel@redhat.com )
 
 """
 The sax module contains a collection of classes that provide a (D)ocument
 (O)bject (M)odel representation of an XML document. The goal is to provide an
-easy, intuitive interface for managing XML documents. Although, the term, DOM,
-is used above, this model is B{far} better.
+easy, intuitive interface for managing XML documents. Although the term DOM is
+used here, this model is B{far} better.
 
 XML namespaces in suds are represented using a (2) element tuple containing the
 prefix and the URI, e.g. I{('tns', 'http://myns')}
@@ -26,13 +25,12 @@ prefix and the URI, e.g. I{('tns', 'http://myns')}
 @var encoder: A I{pluggable} XML special character processor used to encode/
     decode strings.
 @type encoder: L{Encoder}
+
 """
 
 from suds.sax.enc import Encoder
 
-#
 # pluggable XML special character encoder.
-#
 encoder = Encoder()
 
 
@@ -40,25 +38,25 @@ def splitPrefix(name):
     """
     Split the name into a tuple (I{prefix}, I{name}). The first element in the
     tuple is I{None} when the name does not have a prefix.
+
     @param name: A node name containing an optional prefix.
     @type name: basestring
-    @return: A tuple containing the (2) parts of I{name}
+    @return: A tuple containing the (2) parts of I{name}.
     @rtype: (I{prefix}, I{name})
+
     """
-    if isinstance(name, basestring) and ':' in name:
-        return tuple(name.split(':', 1))
+    if isinstance(name, basestring) and ":" in name:
+        return tuple(name.split(":", 1))
     return None, name
 
 
 class Namespace:
-    """
-    The namespace class represents XML namespaces.
-    """
+    """XML namespace."""
 
     default = (None, None)
-    xmlns = ('xml', 'http://www.w3.org/XML/1998/namespace')
-    xsdns = ('xs', 'http://www.w3.org/2001/XMLSchema')
-    xsins = ('xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+    xmlns = ("xml", "http://www.w3.org/XML/1998/namespace")
+    xsdns = ("xs", "http://www.w3.org/2001/XMLSchema")
+    xsins = ("xsi", "http://www.w3.org/2001/XMLSchema-instance")
     all = (xsdns, xsins)
 
     @classmethod
@@ -72,7 +70,7 @@ class Namespace:
     @classmethod
     def xsd(cls, ns):
         try:
-            return cls.w3(ns) and ns[1].endswith('XMLSchema')
+            return cls.w3(ns) and ns[1].endswith("XMLSchema")
         except Exception:
             pass
         return False
@@ -80,7 +78,7 @@ class Namespace:
     @classmethod
     def xsi(cls, ns):
         try:
-            return cls.w3(ns) and ns[1].endswith('XMLSchema-instance')
+            return cls.w3(ns) and ns[1].endswith("XMLSchema-instance")
         except Exception:
             pass
         return False
@@ -92,7 +90,7 @@ class Namespace:
     @classmethod
     def w3(cls, ns):
         try:
-            return ns[1].startswith('http://www.w3.org')
+            return ns[1].startswith("http://www.w3.org")
         except Exception:
             pass
         return False
