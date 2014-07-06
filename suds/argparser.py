70014e1646cb293a0737299f8394195dcc65fd46
Jurko Gospodneti? <jurko.gospodnetic@pke.hr>
2014-07-06 19:59:00 +0200

    fix typo in the main argparser.py module comment

--- suds/argparser.py
+++ suds/argparser.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-# written by: Jurko Gospodnetić jurko.gospodnetic@pke.hr )
+# written by: Jurko Gospodnetić ( jurko.gospodnetic@pke.hr )
 
 """
 Suds web service operation invocation function argument parser.
